e80ea317112dae1ed74bbdc561f02e4905f0d24e
jim-2000 <jimthecaptaionofsill@example.com>
2022-05-23 17:48:36 +0600

    onboarding Screen done

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,128 @@
+import 'dart:developer' show log;
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:travella/pages/Homepage.dart';
+
+class Onboaring extends StatelessWidget {
+  const Onboaring({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SharedPreferences prefs;
+
+    isFirstTime() async {
+      prefs = await SharedPreferences.getInstance();
+      prefs.setBool('isFirsttime', false);
+    }
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+              image: AssetImage('assets/img/2.jpeg'), fit: BoxFit.cover),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        "CREATE BETTER TOGETHER ",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey.shade800,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      const SizedBox(
+                        width: double.infinity,
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Text(
+                            "Write your Everything now. ",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w900,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Container(
+                        height: 8,
+                        width: 45,
+                        decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(10)),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: double.infinity,
+                child: Center(
+                  child: Text(
+                    "It's free and easy now. ",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  isFirstTime();
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (ctx) => const Homepage()));
+                },
+                child: Container(
+                  height: 55,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        offset: Offset.zero,
+                        spreadRadius: 2,
+                        color: Colors.orange.shade500,
+                      )
+                    ],
+                  ),
+                  child: Center(
+                      child: Text(
+                    "START WRITE",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
